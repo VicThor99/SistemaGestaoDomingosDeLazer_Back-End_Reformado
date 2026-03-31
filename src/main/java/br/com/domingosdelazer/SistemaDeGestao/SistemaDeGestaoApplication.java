2a1c8e01ae59ba87d677af35672a1edd28217f78
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2026-03-31 20:45:02 -0300

    initial commit

---
+++ src/main/java/br/com/domingosdelazer/SistemaDeGestao/SistemaDeGestaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.domingosdelazer.SistemaDeGestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/domingosdelazer/SistemaDeGestao/SistemaDeGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.domingosdelazer.SistemaDeGestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
